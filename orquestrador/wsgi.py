3cd7c9cc3f95333074a7a0283466b55dc1d20146
Kelvin Santos <kelvinsantosdealmeida@gmail.com>
2026-04-18 13:37:58 -0300

    chat fixed

--- orquestrador/wsgi.py
+++ orquestrador/wsgi.py
@@ -1,29 +1,29 @@
-from flask import Flask
+# from flask import Flask
 
-from extensions import socketio
+# from extensions import socketio
 
-app = Flask(__name__)
-app.secret_key = "sua_chave_super_secreta"
-app.config['SECRET_KEY'] = "sua_chave_super_secreta"
-socketio.init_app(app)
+# app = Flask(__name__)
+# app.secret_key = "sua_chave_super_secreta"
+# app.config['SECRET_KEY'] = "sua_chave_super_secreta"
+# socketio.init_app(app)
 
-from routes.login import login_bp
-from routes.student import student_bp
-from routes.teacher import teacher_bp
-from routes.session import session_bp
-from routes.strategies import strategy_bp
-from routes.domain import domain_bp
-from routes.orchestrator.agente_strategies.agete_strategies_routes import agete_strategies_bp
-from routes.orchestrator.agente_user.agente_user_routes import agete_user_bp
+# from routes.login import login_bp
+# from routes.student import student_bp
+# from routes.teacher import teacher_bp
+# from routes.session import session_bp
+# from routes.strategies import strategy_bp
+# from routes.domain import domain_bp
+# from routes.orchestrator.agente_strategies.agete_strategies_routes import agete_strategies_bp
+# from routes.orchestrator.agente_user.agente_user_routes import agete_user_bp
 
-app.register_blueprint(login_bp)
-app.register_blueprint(student_bp)
-app.register_blueprint(teacher_bp)
-app.register_blueprint(session_bp)
-app.register_blueprint(strategy_bp)
-app.register_blueprint(domain_bp)
-app.register_blueprint(agete_strategies_bp)
-app.register_blueprint(agete_user_bp)
+# app.register_blueprint(login_bp)
+# app.register_blueprint(student_bp)
+# app.register_blueprint(teacher_bp)
+# app.register_blueprint(session_bp)
+# app.register_blueprint(strategy_bp)
+# app.register_blueprint(domain_bp)
+# app.register_blueprint(agete_strategies_bp)
+# app.register_blueprint(agete_user_bp)
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
